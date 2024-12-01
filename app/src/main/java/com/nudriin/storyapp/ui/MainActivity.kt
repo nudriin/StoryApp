82018fae2ea2f1c2bedc6f523f1e1639edf44e0c
Nurdin <mrsunnysummer@gmail.com>
2024-12-01 22:37:47 +0700

    chore: move main activity to ui package

--- app/src/main/java/com/nudriin/storyapp/ui/MainActivity.kt
+++ app/src/main/java/com/nudriin/storyapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.nudriin.storyapp
+package com.nudriin.storyapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.nudriin.storyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
