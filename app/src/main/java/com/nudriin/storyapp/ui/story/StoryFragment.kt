c9874f8727afb83c8c68e3df6286c432b9a4d98b
Nurdin <mrsunnysummer@gmail.com>
2024-12-02 05:20:56 +0700

    feat: add story page functionality to get all stories

--- app/src/main/java/com/nudriin/storyapp/ui/story/StoryFragment.kt
+++ app/src/main/java/com/nudriin/storyapp/ui/story/StoryFragment.kt
@@ -5,12 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nudriin.storyapp.R
+import com.nudriin.storyapp.adapter.StoryAdapter
+import com.nudriin.storyapp.common.StoryViewModel
+import com.nudriin.storyapp.data.dto.response.ListStoryItem
 import com.nudriin.storyapp.databinding.FragmentStoryBinding
+import com.nudriin.storyapp.utils.MyResult
+import com.nudriin.storyapp.utils.ViewModelFactory
+import com.nudriin.storyapp.utils.showToast
 
 class StoryFragment : Fragment() {
     private var _binding: FragmentStoryBinding? = null
     private val binding get() = _binding!!
+    private val homeViewModel: StoryViewModel by viewModels {
+        ViewModelFactory.getInstance(requireContext())
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,12 +39,49 @@ class StoryFragment : Fragment() {
     }
 
     private fun setupView() {
+        val layoutManager = LinearLayoutManager(context)
+        binding.rvStory.layoutManager = layoutManager
 
+        homeViewModel.getAllStories().observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is MyResult.Loading -> {
+                    showLoading(true)
+                }
+
+                is MyResult.Success -> {
+                    showLoading(false)
+                    setStoriesList(result.data.listStory)
+                }
+
+                is MyResult.Error -> {
+                    showLoading(false)
+                    showToast(requireContext(), result.error)
+                }
+            }
+        }
     }
 
     private fun setupAction() {
         binding.btnAdd.setOnClickListener {
 
+        }
+    }
+
+    private fun setStoriesList(storiesList: List<ListStoryItem>) {
+        val adapter = StoryAdapter(storiesList)
+        binding.rvStory.adapter = adapter
+        adapter.setOnItemClickCallback(object : StoryAdapter.OnItemClickCallback {
+            override fun onItemClicked(storyId: String) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
         }
     }
 
