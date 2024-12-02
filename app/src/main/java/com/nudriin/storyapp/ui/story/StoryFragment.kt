0a7aa37777295ca4d05b1ad5dde75e330ec92c0f
Nurdin <mrsunnysummer@gmail.com>
2024-12-03 01:19:00 +0700

    changes: story fragment to add story activity intent flags

--- app/src/main/java/com/nudriin/storyapp/ui/story/StoryFragment.kt
+++ app/src/main/java/com/nudriin/storyapp/ui/story/StoryFragment.kt
@@ -90,8 +90,6 @@ class StoryFragment : Fragment() {
 
         binding.btnAdd.setOnClickListener {
             val intent = Intent(requireContext(), AddStoryActivity::class.java)
-            intent.flags =
-                Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
     }
